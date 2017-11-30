09008427abf73369e7809727f07ab256a8398130
Santiago Basulto <santiago.basulto@gmail.com>
2017-11-30 14:47:49 -0300

    Solutions

--- assignment_2/main.py
+++ assignment_2/main.py
@@ -1,2 +1,8 @@
 def calculate_purchase_price(purchase, set_to_dict=False):
-    pass
+    total = 0
+    books = purchase['books']
+    for book in books:
+        total += book['price']
+    if set_to_dict:
+        purchase['total'] = total
+    return total
--- assignment_3/main.py
+++ assignment_3/main.py
@@ -1,2 +1,8 @@
 def number_of_customers_per_state(customers_dict):
-    pass
+    results = {}
+    for state, customers in customers_dict.items():
+        if customers:
+            results[state] = len(customers)
+        else:
+            results[state] = 0
+    return results
