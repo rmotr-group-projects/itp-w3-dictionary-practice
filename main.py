8af4d6b4d8e8c3445dfdadcf1d2805d6512436be
Santiago Basulto <santiago.basulto@gmail.com>
2017-11-30 23:53:08 -0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from football_dictionaries.squads_data import SQUADS_DATA
-from football_dictionaries.assignment_1 import players_as_dictionaries
-from pprint import pprint
-
-
-result = players_as_dictionaries(SQUADS_DATA)
-pprint(result)
