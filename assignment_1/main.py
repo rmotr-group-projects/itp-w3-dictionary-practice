f046204b4e04e50e374fbfbfaf59b2eca6265b5b
nandadobal <nanda.dobal@gmail.com>
2017-12-03 17:18:30 +0000

    Update main.py

--- assignment_1/main.py
+++ assignment_1/main.py
@@ -1,3 +1,10 @@
 def add_book_to_purchase(purchase_dict, title, author, price=0.99):
-    pass
-#test
+    listofbooks = purchase_dict['books']
+    newbook = {}
+    newbook['title'] = title
+    newbook['author'] = author
+    newbook['price'] = price
+    listofbooks.append(newbook)
+    purchase_dict['books'] = listofbooks
+    return purchase_dict
+
