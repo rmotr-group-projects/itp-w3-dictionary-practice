b102d59367b499fbd3481228578b83815a21b0cf
Akshay Chaudhary <akshaych7@hotmail.com>
2017-12-08 05:44:10 +0000

    Added solution to Assignment 3

--- assignment_3/main.py
+++ assignment_3/main.py
@@ -1,2 +1,12 @@
 def number_of_customers_per_state(customers_dict):
-    pass
+  result = {}
+  
+  for state, state_customer in customers_dict.items():
+    if state_customer is None:
+      result[state] = 0
+      
+    else: 
+      result[state] = len(state_customer)
+      
+  return result
+  
