c8dc86eaed67be3465798118ab42425bc44629c1
Nick T <njtolomei@gmail.com>
2017-12-08 17:07:04 +0000

    Solutions No. 1

--- assignment_4/main.py
+++ assignment_4/main.py
@@ -1,2 +1,13 @@
 def eldest_customer_per_state(customers_dict):
-    pass
+    eldest_customer = None
+    for customer in customers_list:
+        if eldest_customer is None:
+            eldest_customer = customer
+
+        else:
+            customer_age = customer['age']
+            eldest_customer_age = eldest_customer['age']
+            if customer_age >= eldest_customer_age:
+                eldest_customer = customer
+
+    return eldest_customer
