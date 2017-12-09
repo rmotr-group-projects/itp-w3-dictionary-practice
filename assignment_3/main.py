a85deb545fe1cbdb014abc83a1d2e05baddc2285
Shimanti Guha <shimanti@gmail.com>
2017-12-09 05:30:14 +0000

    Last assignment not working

--- assignment_3/main.py
+++ assignment_3/main.py
@@ -1,2 +1,8 @@
-def number_of_customers_per_state(customers_dict):
-    pass
+def number_of_customers_per_state(customers):
+    results = {}
+    for state, state_customers in customers.items():
+        if state_customers is None:
+            results[state] = 0
+        else:
+            results[state] = len(state_customers)
+    return results
--- assignment_4/main.py
+++ assignment_4/main.py
@@ -1,2 +1,9 @@
-def eldest_customer_per_state(customers_dict):
-    pass
+def eldest_customer_per_state(customers):
+    result = {}
+    for state, state_customers in customers.items():
+        eldest_customer = None
+        for customer in customers:
+            if eldest_customer is None or customer['age'] >= eldest_customer['age']:
+                eldest_customer = customer
+        result[state] = eldest_customer
+    return result
