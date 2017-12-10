abd3b49eb8e7f492f66196790b1d9e22f2030d27
Fernanda <fernanda.dobal@network.rca.ac.uk>
2017-12-10 10:34:48 +0000

    FD dictionary exercises

--- assignment_3/main.py
+++ assignment_3/main.py
@@ -1,2 +1,9 @@
 def number_of_customers_per_state(customers_dict):
-    pass
+    newdict = {}
+    for key, value in customers_dict.items():
+        if value != None:
+            newdict[key] = len(value)
+        else:
+            newdict[key] = 0
+    return newdict
+        
--- assignment_4/main.py
+++ assignment_4/main.py
@@ -1,2 +1,9 @@
 def eldest_customer_per_state(customers_dict):
-    pass
+    newdict = {}
+    for state, customers in customers_dict.items():
+        oldest = None
+        for customer in customers:
+            if oldest == None or customer['age'] > oldest['age']:
+                oldest = customer
+        newdict[state] = oldest
+    return newdict
