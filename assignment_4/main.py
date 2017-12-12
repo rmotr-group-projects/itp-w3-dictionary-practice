ffdacaa7c084190cb5848cc288ff99fdf7ede5be
Jordan Winfery <jwinf843@gmail.com>
2017-12-12 01:21:30 +0000

    Final Answer to Assignments

--- assignment_4/main.py
+++ assignment_4/main.py
@@ -1,2 +1,20 @@
 def eldest_customer_per_state(customers_dict):
-    pass
+    answer = {}
+    for state, value in customers_dict.items():
+        if value == None:
+            answer[state] = None
+        else:
+            oldest = None
+            for customer in value:
+                name = customer['name']
+                age = customer['age']
+                
+                if oldest is None:
+                    oldest = customer
+                else:
+                    oldest_age = oldest['age']
+                    if age >= oldest_age:
+                        oldest = customer
+            answer[state] = oldest
+    print(answer)
+    return answer
