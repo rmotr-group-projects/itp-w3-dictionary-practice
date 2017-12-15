630d1715614abaa21ba137e06049653730ef898d
Shimanti Guha <shimanti@gmail.com>
2017-12-15 04:14:06 +0000

    updating Shimantis file

--- assignment_4/main.py
+++ assignment_4/main.py
@@ -1,9 +1,13 @@
+def eldest_customer_in_list(customers_list):
+    eldest_customer = None # {'age': 1, 'name': 'Empty Customer'}
+    for customer in customers_list:
+        if eldest_customer is None or customer['age'] >= eldest_customer['age']:
+            eldest_customer = customer
+    return eldest_customer
+
 def eldest_customer_per_state(customers):
     result = {}
     for state, state_customers in customers.items():
-        eldest_customer = None
-        for customer in customers:
-            if eldest_customer is None or customer['age'] >= eldest_customer['age']:
-                eldest_customer = customer
+        eldest_customer = eldest_customer_in_list(state_customers)
         result[state] = eldest_customer
     return result
