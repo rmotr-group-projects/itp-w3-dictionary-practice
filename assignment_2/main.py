b19bf090ab18474239a2bfecc21e73ece7b34af8
Nick T <njtolomei@gmail.com>
2017-12-18 15:09:33 +0000

    Another Round of Solutions

--- assignment_2/main.py
+++ assignment_2/main.py
@@ -1,2 +1,10 @@
 def calculate_purchase_price(purchase, set_to_dict=False):
-    pass
+    total = 0
+    for book in purchase['books']:
+        total += book['price']
+
+    if set_to_dict is True:
+        purchase['total'] = total
+
+    return total
+
